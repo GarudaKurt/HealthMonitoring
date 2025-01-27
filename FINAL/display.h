dc6c1495409c3c841dde95f7f212a69931ad2dbe
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-01-28 07:42:36 +0800

    Update display.h

--- FINAL/display.h
+++ FINAL/display.h
@@ -7,5 +7,8 @@
 void initLCD();
 void updateDisplay(double temp, int32_t spo2, int32_t heartRate, int32_t resplvl);
 void displayWarnHR(const char * message, const char * messaga1);
+void clearDisplay();
+void displayGraph(int32_t heartRate);
+
 
 #endif
