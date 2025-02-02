8416bc9cb4a5f8decd32cdde12b24049228b9685
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-02-02 21:24:21 +0800

    Update main.ino

--- FINAL/main.ino
+++ FINAL/main.ino
@@ -38,25 +38,22 @@ void loop() {
     int32_t spo2 = spo2Value();
     int32_t respiratoryRate = calculateRespiratoryRate();
     hr = readECGHr();
-    
+
     updateDisplay(temp, spo2, hr, respiratoryRate ); 
     
     Serial.print("Body Temp: ");
-    Serial.printlnI(readTemperature());
+    Serial.println(readTemperature());
 
     Serial.print("respiratoryRate: ");
-    Serial.printlnI(calculateRespiratoryRate());
+    Serial.println(calculateRespiratoryRate());
 
     Serial.print("Heart Rate: ");
-    Serial.printlnI(readECGHr());
+    Serial.println(readECGHr());
     
     Serial.println(spo2Value());
     Serial.print("Validate: ");
     Serial.println(spo2Validate());
     Serial.print("SPO2: ");
-
-    if(spo2Validate == 0)
-      spo2 = 0;
 
     if(temp > 37.50) {
       displayWarnHR(temp, "Temp");
